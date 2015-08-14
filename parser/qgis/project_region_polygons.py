bb6739bc83349b4aa803a42590b1d7c5c1b6ea5e
martind <martin@dekstop.de>
2015-08-14 01:05:57 +0000

    parser tweaks

--- parser/qgis/project_region_polygons.py
+++ parser/qgis/project_region_polygons.py
@@ -15,11 +15,14 @@ import PyQt4.QtCore
 
 # datadir = dialog.getOpenFileName(None, "Input directory with */tasks.json GeoJSON files")
 # outdir = dialog.getOpenFileName(None, "Output directory for shapefile")
-datadir = '/home/martin/osm/outputs/20150323-hot-scraper/data'
-outdir = '/home/martin/osm/outputs/20150323-hot-scraper/shapefiles'
+datadir = '/Users/mongo/osm/data/etl/hotosm_history_20150813/hotosm-tm2/data'
+outdir = '/Users/mongo/osm/data/etl/hotosm_history_20150813/hotosm-tm2/shapefiles'
+
+if not os.path.exists(outdir):
+  os.makedirs(outdir)
 
 infilespec = '%s/*/tasks.json' % datadir
-tempfilename = '%s/temp_dissolved.shp' % outdir
+diss_shapefilename = '%s/temp_dissolved.shp' % outdir
 outlayername = 'hot_project_regions'
 outfilename = '%s/%s.shp' % (outdir, outlayername)
 
@@ -34,6 +37,12 @@ for infilename in glob.glob(infilespec):
   print infilename
   
   id = os.path.basename(os.path.dirname(infilename))
+  if id=='738' or id=='1020': 
+    # 738: project never launched, and shapefile crashes QGIS
+    # 1020: wrong CRS
+    print "Skipping this one..."
+    continue
+
   layer = qgis.utils.iface.addVectorLayer(infilename, 'tasks-%s' % id, 'ogr')
   if layer==None:
     print "Failed to load the layer! Check its file format."
@@ -45,13 +54,13 @@ for infilename in glob.glob(infilespec):
     QgsVectorFileWriter.deleteShapeFile(outfilename) # clean previous runs
     writer = QgsVectorFileWriter(outfilename, None, fields,
       provider.geometryType(), layer.crs())
-  
-  QgsGeometryAnalyzer().dissolve(layer, tempfilename, onlySelectedFeatures=False, uniqueIdField=-1, p=None)
+ 
+  QgsGeometryAnalyzer().dissolve(layer, diss_shapefilename, onlySelectedFeatures=False, uniqueIdField=-1, p=None)
   QgsMapLayerRegistry.instance().removeMapLayers([layer.id()])
-  layer = qgis.utils.iface.addVectorLayer(tempfilename, 'tasks-%s-diss' % id, 'ogr')
+  diss_layer = qgis.utils.iface.addVectorLayer(diss_shapefilename, 'tasks-%s-diss' % id, 'ogr')
   
   try:
-    for feature in processing.features(layer):
+    for feature in processing.features(diss_layer):
       outGeom = QgsGeometry(feature.geometry())
       outFeat = QgsFeature(fields)
       outFeat.setGeometry(outGeom)
@@ -62,7 +71,8 @@ for infilename in glob.glob(infilespec):
   except Exception as e:
     print e
   
-  QgsMapLayerRegistry.instance().removeMapLayers([layer.id()])
+  QgsMapLayerRegistry.instance().removeMapLayers([diss_layer.id()])
+  QgsVectorFileWriter.deleteShapeFile(diss_shapefilename)
 
 del writer
 
